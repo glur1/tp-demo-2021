f0cb9b9a031bc3c07b763f315bb2bf65064c48a5
Aurélien Moreau <aurelien.moreau@univ-rennes1.fr>
2021-09-22 09:20:31 +0200

    add uml and money

---
+++ src/main/java/demo/Money.java
@@ -0,0 +1,40 @@
+package demo;
+
+import java.util.Objects;
+
+public class Money {
+
+    private int amount;
+    private String currency;
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    @Override
+    public String toString() {
+        return "Money{" +
+                "amount=" + amount +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
+
+    public Money add(Money that) {
+        return new Money(this.amount+that.amount,this.currency);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money = (Money) o;
+        return amount == money.amount &&
+                Objects.equals(currency, money.currency);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount, currency);
+    }
+}
