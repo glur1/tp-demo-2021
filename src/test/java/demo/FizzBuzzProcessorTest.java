526417c8cbe523bf2389209aae311bfb8f407a01
Aurélien Moreau <aurelien.moreau@univ-rennes1.fr>
2021-09-26 22:28:33 +0200

    evo conf

--- src/test/java/demo/FizzBuzzProcessorTest.java
+++ src/test/java/demo/FizzBuzzProcessorTest.java
@@ -9,28 +9,28 @@ public class FizzBuzzProcessorTest {
     public void FizzBuzzNormalNumbers() {
 
         FizzBuzzProcessor fb = new FizzBuzzProcessor();
-        Assert.assertEquals("1", fb.convert(1));
-        Assert.assertEquals("2", fb.convert(2));
+        Assert.assertEquals("1", FizzBuzzProcessor.convert(1));
+        Assert.assertEquals("2", FizzBuzzProcessor.convert(2));
     }
 
     @Test
     public void FizzBuzzThreeNumbers() {
 
         FizzBuzzProcessor fb = new FizzBuzzProcessor();
-        Assert.assertEquals("Fizz", fb.convert(3));
+        Assert.assertEquals("Fizz", FizzBuzzProcessor.convert(3));
     }
 
     @Test
     public void FizzBuzzFiveNumbers() {
 
         FizzBuzzProcessor fb = new FizzBuzzProcessor();
-        Assert.assertEquals("Buzz", fb.convert(5));
+        Assert.assertEquals("Buzz", FizzBuzzProcessor.convert(5));
     }
 
     @Test
     public void FizzBuzzThreeAndFiveNumbers() {
 
         FizzBuzzProcessor fb = new FizzBuzzProcessor();
-        Assert.assertEquals("Buzz", fb.convert(5));
+        Assert.assertEquals("FizzBuzz", FizzBuzzProcessor.convert(15));
     }
 }
